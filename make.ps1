11dae08ba76c16a70e6c705f0666c3bd385f800a
Myroslav Rys <stonevil@gmail.com>
2015-02-10 15:32:01 +0200

    Add packer execution procedure

--- make.ps1
+++ make.ps1
@@ -23,9 +23,9 @@ Param(
     $GENERALIZE = "false")
 
 Write-Host "Target = $TARGET"
+Write-Host "Update_OS = $UPDATE"
 Write-Host "Configuration_Management = $CM"
 Write-Host "Configuration_Management_Version = $CM_VERSION"
-Write-Host "Update_OS = $UPDATE"
 Write-Host "Box_Version = $BOX_VERSION"
 Write-Host "Generalize = $GENERALIZE"
 
@@ -38,7 +38,27 @@ $PACKER_VARS += "-var cm=$CM -var update=$UPDATE"
 # $PACKER_VARS += "-var cm_version=$CM_VERSION"
 #}
 
-
 $PACKER_VARS
 
+$proc = New-Object System.Diagnostics.ProcessStartInfo
+$proc.FileName = "packer.exe"
+$proc.RedirectStandardError = $true
+$proc.RedirectStandardOutput = $true
+$proc.UseShellExecute = $false
+$proc.Arguments = "build -only=hyper-iso $PACKER_VARS $TARGET.json"
+$proc.Vars = "runAs"
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $proc
+$p.Start() | Out-Null
+$p.WaitForExit()
+$stdout = $p.StandardOutput.ReadToEnd()
+$stderr = $p.StandardError.ReadToEnd()
+Write-Host "stdout: $stdout"
+Write-Host "stderr: $stderr"
+Write-Host "exit code: " + $p.ExitCode
+
+
+# Envoke packer with params
+# $packerbin = "packer.exe"
+# $packerarguments = "build -only=hyper-iso $PACKER_VARS $TARGET.json"
 # start-process packer -ArgumentList build -only=hyperv-iso $PACKER_VARS $TARGET.json -verb runAs
