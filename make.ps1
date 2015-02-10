bf77052ca8cfa505ce7aaebbef730c9dec1e6662
Myroslav Rys <stonevil@gmail.com>
2015-02-10 14:29:25 +0200

    Remove not required check

--- make.ps1
+++ make.ps1
@@ -1,17 +1,45 @@
-$Env:UPDATE
+# Parse command line arguments
+Param(
+    [Parameter(Position=1)]
+    [alias("t")]
+    $TARGET =,
+    [Parameter(Position=2)]
+    [ValidateSet("nocm","chef","chefdk","salt","puppet")]
+    [alias("c")]
+    $CM = "nocm",
+    [Parameter(Position=3)]
+    [alias("v")]
+    $CM_VERSION = "latest",
+    [Parameter(Position=4)]
+    [ValidateSet("true","false")]
+    [alias("u")]
+    $UPDATE = "false",
+    [Parameter(Position=5)]
+    [alias("b")]
+    $BOX_VERSION,
+    [Parameter(Position=6)]
+    [ValidateSet("true","false")]
+    [alias("g")]
+    $GENERALIZE = "false")
 
-# Override variables. Just for testing
-if ($Env:UPDATE = "true")
-if ($Env:UPDATE = "nocm")
+Write-Host "Target = $TARGET"
+Write-Host "Configuration_Management = $CM"
+Write-Host "Configuration_Management_Version = $CM_VERSION"
+Write-Host "Update_OS = $UPDATE"
+Write-Host "Box_Version = $BOX_VERSION"
+Write-Host "Generalize = $GENERALIZE"
 
-if ($Env:UPDATE -eq "true")
-{
- $PACKER_VARS += "-var update=update"
-}
 
-if ($Env:CM -eq "nocm")
-{
- $PACKER_VARS += "-var update=nocm"
-}
+# Check variables and generate command line for packer
+$PACKER_VARS += "-var cm=$CM -var update=$UPDATE"
 
-$Env.PACKER_VARS
+#if ($CM_VERSION -neq "")
+#{
+# $PACKER_VARS += "-var cm_version=$CM_VERSION"
+#}
+
+
+$PACKER_VARS
+
+# start-process powershell -verb runAs
+# packer build -only=hyperv-iso $PACKER_VARS $TARGET.json
